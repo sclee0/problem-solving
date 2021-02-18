dbd4cff9075f4f8a7591dfca1fe071687dcb8901
sclee <timber0436>
2021-02-18 17:34:37 +0900

    added the wordcount

---
+++ src/main/java/backjoon/steps/strings/WordCount.java
@@ -0,0 +1,26 @@
+package backjoon.steps.strings;
+
+import java.util.Scanner;
+
+public class WordCount {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine().trim();
+        String []words = s.split(" ");
+
+        int len = words.length;
+
+        if(words.length==1){
+            if(words[0]=="")
+                System.out.println(0);
+            else{
+                System.out.println(len);
+            }
+        }
+        else{
+
+            System.out.println(len);
+        }
+    }
+}
